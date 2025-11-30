5a1740dfe38496dca07c44f4ed5d99a120f39108
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-30 10:53:53 +0530

    Refactor to use recursive function for Fibonacci series

--- fibnocci_series_using_functions.c
+++ fibnocci_series_using_functions.c
@@ -1,22 +1,27 @@
-#include<stdio.h>
-int fib_n(int );
-int main(){
-    int n,fib;
-    printf("Enter factorial numeber: ");
-    scanf("%d",&n);
- fib=fib_n(n);
-    printf("factoral of numbers: %d\n",fib);
+#include <stdio.h>
+
+void fib_series(int n, int a, int b) {
+    if (n == 0) {
+        return;            // base condition: no more terms to print
+    }
+
+    printf("%d ", a);      // print current term
+    fib_series(n - 1, b, a + b);  // recursive call with next terms
+}
+
+int main() {
+    int n;
+
+    printf("Enter how many Fibonacci numbers you want: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Please enter a positive number.\n");
+        return 0;
+    }
+
+    fib_series(n, 0, 1);   // start with 0 and 1
+    printf("\n");
 
     return 0;
-
-}
-int fib_n(int n){
-    int i;
-    int a=0,b=1,s=1;
-    for(i=1;i<=n;i--){
-        s=a+b;
-        a=b;
-        b=s;
-    }
-    return s;
 }
